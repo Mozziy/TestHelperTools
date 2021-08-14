27facd481426e9d024c77c163f8793bcba11a3ea
Mozziy <1248623976@qq.com>
2021-08-14 14:32:26 +0800

    日志查询功能界面绘制

--- src/components/LogQuery.vue
+++ src/components/LogQuery.vue
@@ -1,13 +1,75 @@
 <template>
-  <h1>日志查询组件</h1>
+  <div id="lq">
+    <el-input
+      placeholder="请输入键值后点击查询"
+      prefix-icon="el-icon-search"
+      v-model="input1"
+      class="input-with-select"
+    >
+      <template #prepend>
+        <el-select v-model="value" placeholder="环境选择"
+        default-first-option clearable filterable>
+          <el-option v-for="item in EnvList"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+          </el-option>
+        </el-select>
+        <el-select-v2 v-model="item" placeholder="系统选择" clearable
+        filterable :options="options" style="width: 130px;" default-first-option/>
+      </template>
+      <template #append>
+        <el-button type="primary" icon="el-icon-search">搜索</el-button>
+      </template>
+    </el-input>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'LogQuery'
-}
+import { defineComponent, ref } from 'vue'
+export default defineComponent({
+  setup () {
+    // 系统列表
+    const SysList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+    // 环境列表
+    const EnvList = [{
+      value: 'st',
+      label: 'st'
+    }, {
+      value: 'uat',
+      label: 'uat'
+    }, {
+      value: 'pre',
+      label: 'pre'
+    }]
+    const options = Array.from({ length: SysList.length - 1 }).map((_, idx) => ({
+      item: `选项${idx + 1}`,
+      label: `${SysList[idx]}`
+    }))
+    return {
+      input1: ref(''),
+      select: ref(''),
+      value: ref(''),
+      item: [],
+      options,
+      EnvList
+    }
+  }
+})
 </script>
 
-<style lang="less" scoped>
+<style>
+.el-select .el-input {
+  width: 130px;
+}
 
+.input-with-select .el-input-group__prepend {
+  background-color: #fff;
+  padding-left: 0;
+  padding-right: 0;
+
+}
+.el-select-v2 {
+  margin-inline-start: 15px;
+}
 </style>
