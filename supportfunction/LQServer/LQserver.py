44ce54722b566a64f825cda0ad729f91f5ea723f
Mozziy <1248623976@qq.com>
2021-08-15 18:49:49 +0800

    后端代码优化

--- supportfunction/LQServer/LQserver.py
+++ supportfunction/LQServer/LQserver.py
@@ -7,13 +7,21 @@
 @time: 2021/08/08 18:52
 """
 
-import flask
+from flask import Flask
+from flask_cors import CORS
 from lib.GetLog import GetLog
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
+# 解决跨域问题
+CORS(app, supports_credentials=True)
 
+@app.route('/')
+def lqserver():
 
+    return
 
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=9000, debug=True)
 
 
 
